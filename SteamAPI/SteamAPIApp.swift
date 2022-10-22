ea7c0dec0af617d54c4e5a1695b0ec4471c360e1
Arno van Zyl <vanzylarno@yahoo.com>
2022-10-22 11:30:43 +0200

    Initial Commit

---
+++ SteamAPI/SteamAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteamAPIApp.swift
+//  SteamAPI
+//
+//  Created by Arno van Zyl on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SteamAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
